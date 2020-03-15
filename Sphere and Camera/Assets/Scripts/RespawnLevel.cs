441172a5020c1dae9b3da79db46f1585a767578d
AdusV <adriankomendera@interia.pl>
2020-03-15 10:36:15 +0100

    Dodanie kolejnego respawnu

--- Sphere and Camera/Assets/Scripts/RespawnLevel.cs
+++ Sphere and Camera/Assets/Scripts/RespawnLevel.cs
@@ -6,10 +6,9 @@ public class RespawnLevel : MonoBehaviour
 {
     public GameObject boxPrefab;
     public GameObject wallPrefab;
-   /* public GameObject wall1Prefab;
+    public GameObject wall1Prefab;
     public GameObject wall2Prefab;
-    public GameObject wall3Prefab;*/
-    Vector3 xTurn = new Vector3(-10, 0, 10);
+    public GameObject wall3Prefab;
     Vector3 xNew = new Vector3(10, 0, 10);
     Vector3 zTurn = new Vector3(-10, 0, 0);
     
@@ -17,6 +16,13 @@ public class RespawnLevel : MonoBehaviour
     {
         GameObject wall = GameObject.Instantiate(wallPrefab);
         wall.transform.localEulerAngles = new Vector3(0,90,0);
+        GameObject wall1 = GameObject.Instantiate(wall1Prefab);
+        wall1.transform.localEulerAngles = new Vector3(0, -90, 0);
+        GameObject wall2 = GameObject.Instantiate(wall2Prefab);
+        wall2.transform.localEulerAngles = new Vector3(0, -180, 0);
+        GameObject wall3 = GameObject.Instantiate(wall3Prefab);
+        wall3.transform.localEulerAngles = new Vector3(0, 90, 0);
+        
         /*GameObject.Instantiate(boxPrefab);
         GameObject.Instantiate(wallPrefab);
         GameObject.Instantiate(wall2Prefab);
